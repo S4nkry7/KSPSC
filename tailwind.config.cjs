8927a9fafc4e7d9cb2e741011217592a5b97be5c
S4nkry7 <107291299+S4nkry7@users.noreply.github.com>
2024-07-31 23:26:20 -0600

    Se termina pagina

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,8 +18,24 @@ module.exports = {
         serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
       },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [typographyPlugin, animations],
+  plugins: [typographyPlugin, animations,
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    },
+  ],
   darkMode: 'class',
 };
